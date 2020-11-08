32737f9522df68e06909f20bf015168a95f81a0f
Jamie Hale <jamiesonhale@gmail.com>
2020-11-08 15:32:37 -0500

    First pass

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
+import * as R from 'ramda';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const puzzle = 'az uiwaoys urwlyl, tuio sr nrw dikk lyol re loygl kyiszam wg or uzmuyb ekrrbl? ldiby-tinl.';
+
+const letters = 'abcdefghijklmnopqrstuvwxyz';
+
+const isLetter = (c) => R.includes(c, letters);
+
+const transformed = (puzzle, letterMap) => R.map(
+  R.when(
+    isLetter,
+    (c) => R.propOr(' ', c, letterMap),
+  ),
+  puzzle,
+);
+
+const Letter = styled.div`
+  border: 1px solid black;
+  border-radius: 4px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Stack = styled(({ horizontal, ...props }) => <div {...props} />)`
+  display: flex;
+  flex-direction: ${({ horizontal }) => horizontal ? 'row' : 'column'};
+
+  > * {
+    padding-top: 8px;
+  }
+`;
+
+const Pair = ({
+  letter,
+  solution,
+}) => (
+  <Stack>
+    <Letter>{letter}</Letter>
+    <Letter>{solution}</Letter>
+  </Stack>
+);
+  
+const UnstyledWord = ({
+  className,
+  word,
+  letterMap,
+}) => {
+  const pairs = R.addIndex(R.map)(
+    (letter, i) => <Pair key={i} letter={letter} solution={R.propOr(' ', letter, letterMap)} />,
+    word,
   );
-}
+
+  return (
+    <Stack horizontal className={className}>
+      {pairs}
+    </Stack>
+  );
+};
+
+const Word = styled(UnstyledWord)``;
+
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+
+  ${Word} {
+    margin-right: 32px;
+  }
+`;
+
+const App = () => {
+  const letterMap = {
+    u: 'h',
+  };
+  const puzzleWords = R.split(' ', puzzle);
+  console.log({ puzzleWords });
+  const board = transformed(puzzle, letterMap);
+
+  const words = R.addIndex(R.map)(
+    (word, i) => <Word key={i} word={word} letterMap={letterMap} />,
+    puzzleWords,
+  );
+
+  return (
+    <>
+    <p>I have a ham radio</p>
+    <p>{puzzle}</p>
+    <p>{board}</p>
+    <Container>{words}</Container>
+    </>
+  );
+};
 
 export default App;
